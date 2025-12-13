cbe4a2a46b5e2cba6a7116b915043840d40e2e53
maxijer <maxijer@MacBook-Air-maxijer.local>
2025-12-13 17:28:43 +0300

    init commit

---
+++ app.c
@@ -0,0 +1,16 @@
+#include "app.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+static void handle_file_mode(const char *filename);
+static void handle_sort_once(void);
+static void handle_benchmark(void);
+
+int run_app(int argc, char *argv[])
+{
+    return 0;
+}
+
